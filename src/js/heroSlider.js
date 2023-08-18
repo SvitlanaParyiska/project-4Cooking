874c2f7453f235e09e5629f469801293fc92f2a7
Daria3025 <dtimofeeva191@icloud.com>
2023-08-18 22:23:14 +0300

    half-of-slider

--- src/js/heroSlider.js
+++ src/js/heroSlider.js
@@ -1,1 +1,53 @@
 import Swiper from "swiper";
+import 'swiper/css';
+import axios from "axios";
+
+
+const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api';
+const END_POINT = '/events';
+
+async function fetchImgForSlider() {
+    try {
+        const response = await axios.get(`${BASE_URL}${END_POINT}`);
+        const data = response.data;
+        return data;
+        console.log(data);
+    } catch (err) {
+        console.warn(err)
+    }
+};
+console.log('hello');
+
+fetchImgForSlider();
+
+async function fetchSlider() {
+    const response = await fetchImgForSlider();
+    console.dir(response);
+};
+
+fetchSlider();
+
+function createMarkup(arr) {
+    const markupImg = arr
+    .map(
+        (name, imgUrl) => `
+        <div class="swiper-slide"><ul>
+        <li><img src="" alt=""></li>
+        <li><img src="" alt=""></li>
+        <li><img src="" alt=""></li>
+    </ul></div>
+        `
+    )
+    .join('');
+    return markupImg;
+};
+
+const swiper = new Swiper('.swiper', {
+    direction: 'vertical',
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+});
