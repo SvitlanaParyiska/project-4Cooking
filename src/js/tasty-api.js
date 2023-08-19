b80cb92feca16f5e72ae7520239f8e03f8294a1b
SvitlanaParyiska <paryiskasvitlana@gmail.com>
2023-08-19 12:02:43 +0300

    Update tasty-api.js

--- src/js/tasty-api.js
+++ src/js/tasty-api.js
@@ -1,1 +1,17 @@
-export const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api';
+// export const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api';
+import axios from 'axios';
+
+export class TastyAPI {
+  #BASE_URL = 'https://tasty-treats-backend.p.goit.global/api';
+
+  getAllRecipes(page) {
+    const url = `${this.#BASE_URL}/recipes?limit=9&page=${page}`;
+    return axios.get(url).then(resp => resp.data);
+  }
+
+  getEvents() {
+    const url = `${this.#BASE_URL}/events`;
+    return axios.get(url).then(resp => resp.data);
+  }
+}
+///https://tasty-treats-backend.p.goit.global/api/recipes/
