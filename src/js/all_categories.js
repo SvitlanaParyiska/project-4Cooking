a69972ffe1026d36a076e6e6fe6cdc56e457f6e4
Igor9779 <darthhoit97@gmail.com>
2023-08-20 11:49:25 +0300

    js-fix-1

--- src/js/all_categories.js
+++ src/js/all_categories.js
@@ -1,58 +1,135 @@
 import axios from 'axios';
-import Notiflix from 'notiflix';
 import { BASE_URL } from './tasty-api';
 
-const categoryUrl = `${BASE_URL}/categories`;
-const allCategories = document.querySelector('.all-categories-button');
+const categoryUrl =`${BASE_URL}/categories`;
 
-let sort = '';
+export async function fetchCategories() {
+try{
+    const categories = await axios.get(categoryUrl);
+    
+    return categories.data;
 
-function fetchDataAndRender() {
-    const recipesUrl = `${BASE_URL}/recipes`;
-    axios
-        .get(recipesUrl, {
-            params: { category: sort, page: 1, limit: 0 },
-        })
-        .then(response => {
-            if (response.data.totalPages === 0) {
-                Notiflix.Report.warning(
-                    'Sorry, there are no images matching your search query. Please try again'
-                );
-            } else {
-                pictures = [response.data];
-            }
-            renderCardsList(response.data);
-        })
-        .catch(error => {
-            console.log(error);
-            Notiflix.Report.failure(
-                'Error',
-                'Failed to fetch data. Please try again later.',
-                'OK'
-            );
+}catch(error){
+    console.log(error);
+};
+};
+
+async function fetchDataAndRender(category = '') {
+    try {
+        const recipesUrl = `${BASE_URL}/recipes`;
+        const response = await axios.get(recipesUrl, {
+            params: { category, page: 1, limit: 0 },
         });
+
+        const data = response.data;
+        renderCardsList(data);
+
+        return data;
+    } catch (error) {
+        console.log(error);
+        Notiflix.Report.failure(
+            'Error',
+            'Failed to fetch data. Please try again later.',
+            'OK'
+        );
+        return null;
+    }
 }
 
-axios.get(categoryUrl).then(response => {
-    const recipes = document.querySelector('.sidebar');
-    const recipesMarkup = response.data
-        .map(category => `<button class="buttons-nav">${category.name}</button>`)
-        .join('');
-    recipes.innerHTML = recipesMarkup;
 
-    recipes.addEventListener('click', e => {
-        for (const item of response.data) {
-            if (item.name === e.target.innerText) {
-                sort = e.target.innerText;
-                fetchDataAndRender();
-            }
-        }
-    });
-});
+function renderCategoriesMarkup(categories) {
+const categoriesContainer = document.querySelector('.categories-container');
+const categoryList = categoriesContainer.querySelector('.category-list');
 
-allCategories.addEventListener('click', () => {
+const allCategoryButton = document.querySelector('#all-category-btn');
+allCategoryButton.addEventListener('click', () => {
+    const categoryButtons = categoryList.querySelectorAll('.category-btn');
+    categoryButtons.forEach(button => button.classList.remove('active'));
+    allCategoryButton.classList.add('active');
     sort = '';
+    loadPage = 1;
+    pictures = [];
     fetchDataAndRender();
 });
 
-fetchDataAndRender();
+categoryList.innerHTML = '';
+
+categories.forEach(category => {
+    const categoryButton = document.createElement('button');
+    categoryButton.classList.add('category-btn');
+    categoryButton.textContent = category.name;
+
+    categoryButton.addEventListener('click', () => {
+    const categoryButtons = categoryList.querySelectorAll('.category-btn');
+    categoryButtons.forEach(button => button.classList.remove('active'));
+    categoryButton.classList.add('active');
+    sort = category.name;
+    loadPage = 1;
+    pictures = [];
+    fetchDataAndRender();
+    });
+
+    const catItem = document.createElement('li');
+    catItem.classList.add('cat-items');
+    catItem.appendChild(categoryButton);
+    categoryList.appendChild(catItem);
+});
+}
+
+axios.get(categoryUrl)
+.then(response => {
+    const categories = response.data;
+    renderCategoriesMarkup(categories);
+})
+.catch(error => {
+    console.log(error);
+});
+
+
+const refs = {
+categoryList: document.querySelector('.category-list'),
+categoryContainer: document.querySelector('.category-container'),
+allCategoryButton: document.querySelector('.all-category-button'),
+};
+
+refs.categoryContainer.addEventListener('click', onBtnCLick);
+
+let lastClickedBtn = null;
+
+
+function onBtnCLick(event) {
+const Btn = event.target;
+
+if (Btn.nodeName !== 'BUTTON') {
+    return;
+}
+
+if (lastClickedBtn) {
+    lastClickedBtn.classList.remove('active');
+}
+
+if (Btn === refs.allCategoryButton) {
+    removeActiveClassFromAllButtons();
+} else {
+    refs.allCategoryButton.classList.remove('active');
+}
+
+Btn.classList.add('active');
+lastClickedBtn = Btn;
+};
+
+
+function removeActiveClassFromAllButtons() {
+const buttons = refs.categoryList.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.classList.remove('active');
+});
+};
+
+
+refs.categoryList.addEventListener('click', event => {
+if (!event.target.classList.contains('category-btn')) {
+    event.stopPropagation();
+}
+});
