923b3315ce5fc75267c543dfaee80f2e45054f49
Artem303 <125798638+Artem303@users.noreply.github.com>
2023-08-20 11:44:54 +0300

    make slider

--- src/js/header.js
+++ src/js/header.js
@@ -3,13 +3,9 @@ const closeBtnEl = document.querySelector('.mob-menu-close-btn');
 const mobMenuEl = document.querySelector('.mobile-menu');
 const backdropEl = document.querySelector('.backdrop');
 const bodyEl = document.querySelector('body');
-const switchOffEl = document.querySelector('.switch-off');
-const switchOnEl = document.querySelector('.switch-on');
-const checkEl = document.querySelector('.check-span');
 
 burgerEl.addEventListener('click', handlerTogle);
 closeBtnEl.addEventListener('click', handlerTogle);
-checkEl.addEventListener('click', onChangeSwitch);
 
 function handlerTogle() {
   mobMenuEl.classList.toggle('is-open');
@@ -24,9 +20,4 @@ function hendlerCloseMenu() {
   backdropEl.classList.remove('is-open');
 
   backdropEl.removeEventListener('click', hendlerCloseMenu);
-}
-
-function onChangeSwitch() {
-  switchOffEl.classList.toggle('hidden');
-  switchOnEl.classList.toggle('hidden');
 }
